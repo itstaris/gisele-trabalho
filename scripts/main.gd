ad4e5589236916de0b22fddce7443b1edb4bf5e6
Tamires <tamires.ei04@gmail.com>
2024-11-28 00:28:59 -0300

    artes botadas
    coloquei as artes da lala

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,11 +1,11 @@
 extends Node
 
 var speed : float
-const PLAYER_START_POS := Vector2i(467, 968)
+const PLAYER_START_POS := Vector2i(2078, 492)
 const START_SPEED : float = 14.0
 const CAM_START_POS := Vector2i(1266, 585)
+#const FIRE_START_POS := Vector2i(-626, 0)
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	new_game()
 
@@ -14,10 +14,11 @@ func new_game():
 	$CharacterBody2D.velocity = Vector2i(0, 0)
 	$Camera2D.position = CAM_START_POS
 	$ground.position = Vector2i(0, 0)
+	#$fogo.position = FIRE_START_POS
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	speed = START_SPEED
 	
 	$CharacterBody2D.position.x += speed
-	$Camera2D.position.x += speed
+	$Camera2D.position.x += speed + 2
+	#$fogo.position.x += speed + 2
